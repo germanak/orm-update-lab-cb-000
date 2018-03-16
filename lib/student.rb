70b9d6ea4784bfbe18b763e7dcfa40c27c4a61e0
Alec German <alec_german@yahoo.com>
2018-03-16 21:55:32 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,6 +64,6 @@ class Student
       LIMIT 1;
     SQL
 
-    DB[:conn].execute(sql, student_name).map {|row| self.new_from_db(row)}
+    DB[:conn].execute(sql, student_name).map {|row| self.new_from_db(row)}.first
   end
 end
